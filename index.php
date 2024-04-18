6913a1a68d3e3baea67b55d5d18d2195e7254a00
alessiavozzo <a.vozzo95@gmail.com>
2024-04-18 14:16:11 +0200

    add class Production + istantiate 2 objects

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+
+//importo la classe Production
+require_once __DIR__ . "/models/Production.php";
+
+//istanzio 2 oggetti
+$production1 = new Production("Avatar", "english", "10");
+$production2 = new Production("Titanic", "english", "8");
+var_dump($production1, $production2);
--- models/Production.php
+++ models/Production.php
@@ -1,1 +1,16 @@
 <?php
+
+//definisco la class Production
+class Production
+{
+    public $title;
+    public $language;
+    public $rating;
+
+    public function __construct($title, $language, $rating)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
+    }
+}
