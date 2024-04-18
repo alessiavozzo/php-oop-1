f401c1a3f018fc9e5429144639a85eb65b753bed
alessiavozzo <a.vozzo95@gmail.com>
2024-04-18 15:05:39 +0200

    add some partials php + print cards in page with production data

--- db.php
+++ db.php
@@ -10,8 +10,8 @@ $production3 = new Production("San Andreas", "english", "6");
 $production4 = new Production("Dune", "english", "9");
 $production5 = new Production("Matrix", "english", "8");
 $production6 = new Production("Star wars", "english", "10");
-var_dump($production1, $production2, $production3, $production4, $production5, $production6);
+//var_dump($production1, $production2, $production3, $production4, $production5, $production6);
 
 //array di produzioni
 $productionsList = array($production1, $production2, $production3, $production4, $production5, $production6);
-var_dump($productionsList);
+//var_dump($productionsList);
--- index.php
+++ index.php
@@ -8,36 +8,49 @@ $production1 = new Production("Avatar", "english", "10");
 $production2 = new Production("Titanic", "english", "8");
 $production3 = new Production("Titanic", "english", "12");
 var_dump($production1, $production2, $production3); */
-include_once __DIR__ . "/db.php"
+include_once __DIR__ . "/db.php";
+include_once __DIR__ . "/views/head.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productions</title>
-
-    <!-- bs -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <header id="site_header">
-
-    </header>
-
-    <main id="site_main">
-
-    </main>
 
 
+<header id="site_header">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                LOGO
+            </a>
+        </div>
+    </nav>
+</header>
 
-    <!-- scripts -->
-    <!-- bs -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<main id="site_main">
+    <div class="container">
+        <div class="row">
+
+            <!-- foreach loop to print infos in page cards -->
+            <?php foreach ($productionsList as $production) : ?>
+
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $production->title ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted "><?php echo $production->language ?></h6>
+                            <p class="card-text"><?php echo $production->rating ?></p>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
+</main>
+
+
+
+<!-- scripts -->
+<!-- bs -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 
