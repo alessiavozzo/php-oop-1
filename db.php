0235615cd9fee850c1af5efd12b86c349cece6b8
alessiavozzo <a.vozzo95@gmail.com>
2024-04-18 14:50:46 +0200

    add productions array

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+//importo la classe Production
+require_once __DIR__ . "/models/Production.php";
+
+//istanzio 2 oggetti
+$production1 = new Production("Avatar", "english", "10");
+$production2 = new Production("Titanic", "english", "8");
+$production3 = new Production("San Andreas", "english", "6");
+$production4 = new Production("Dune", "english", "9");
+$production5 = new Production("Matrix", "english", "8");
+$production6 = new Production("Star wars", "english", "10");
+var_dump($production1, $production2, $production3, $production4, $production5, $production6);
+
+//array di produzioni
+$productionsList = array($production1, $production2, $production3, $production4, $production5, $production6);
+var_dump($productionsList);
--- index.php
+++ index.php
@@ -1,10 +1,44 @@
 <?php
 
-//importo la classe Production
+/* //importo la classe Production
 require_once __DIR__ . "/models/Production.php";
 
 //istanzio 2 oggetti
 $production1 = new Production("Avatar", "english", "10");
 $production2 = new Production("Titanic", "english", "8");
 $production3 = new Production("Titanic", "english", "12");
-var_dump($production1, $production2, $production3);
+var_dump($production1, $production2, $production3); */
+include_once __DIR__ . "/db.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productions</title>
+
+    <!-- bs -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <header id="site_header">
+
+    </header>
+
+    <main id="site_main">
+
+    </main>
+
+
+
+    <!-- scripts -->
+    <!-- bs -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
